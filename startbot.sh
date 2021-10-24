9e11f54cb59391181e1cd3a26fbe2cae1e3c926e
Omarjeto <92174997+Omarjeto@users.noreply.github.com>
2021-10-24 10:43:12 +0700

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -10,8 +10,8 @@ echo "Creating config.json..."
 cd /home/shell-bot
 cat > config.json << EOF
 {
-    "authToken": "2043757515:AAE4q81nzWwbooc8xoFuGNMH6YkNap03Odk",
-    "owner": 1348352154
+    "authToken": "2086181473:AAHSLFs1Gm08t68FXdBMFFxJ0blcgN_UEJA",
+    "owner": 862535932
 }
 EOF
 echo "Done."
